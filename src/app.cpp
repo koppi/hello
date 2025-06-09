d77d3d54a55a9aefa4d984a004c5a8e08211eba4
Jakob Flierl <jakob.flierl@gmail.com>
2025-06-09 22:58:27 +0200

    foo

--- src/app.cpp
+++ src/app.cpp
@@ -1,4 +1,10 @@
 #include <QApplication>
+#include<QWidget>
+#include<QSlider>
+#include<QSpinBox>
+#include<QHBoxLayout>
+#include<QMenuBar>
+#include<QStatusBar>
 
 #include "win.h"
 
@@ -6,9 +12,48 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    Win w;
+    auto s1 = new QSlider(Qt::Horizontal);
+    auto s2 = new QSlider(Qt::Vertical);
+    auto *sb = new QSpinBox;
 
-    w.show();
+    auto *w = new QMainWindow;
+
+    auto cw = new QWidget(w);
+
+    auto menu = new QMenuBar(cw);
+
+
+    auto *L = new QHBoxLayout(cw);
+    L->addWidget(s1);
+    L->addWidget(s2);
+    L->addWidget(sb);
+
+    QMenu *file = new QMenu("&File");
+    file->addMenu("Open");
+    file->addMenu("Exit");
+
+    QMenu *Build = new QMenu("&Build");
+    Build->addAction("Rebuild this file");
+    Build->addAction("Rebuild All");
+
+    menu->addMenu(file);
+    menu->addMenu(Build);
+
+    w->setCentralWidget(cw);
+
+    w->show();
+
+    QObject::connect  (s1,SIGNAL(valueChanged(int) ),  sb,SLOT(setValue(int) )   );
+    QObject::connect  (s1,SIGNAL(valueChanged(int) ),  s2,SLOT(setValue(int) )   );
+
+    QObject::connect  (s2,SIGNAL(valueChanged(int) ),  sb,SLOT(setValue(int) )   );
+    QObject::connect  (s2,SIGNAL(valueChanged(int) ),  s1,SLOT(setValue(int) )   );
+
+    QObject::connect  (sb,SIGNAL(valueChanged(int) ),  s1,SLOT(setValue(int) )    );
+    QObject::connect  (sb,SIGNAL(valueChanged(int) ),  s2,SLOT(setValue(int) )    );
+
+//    Win w;
+//    w.show();
 
     return QApplication::exec();
 }
