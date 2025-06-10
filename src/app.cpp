9679a0383a6ef7335652f274401478a1556c7641
Jakob Flierl <jakob.flierl@gmail.com>
2025-06-10 08:40:29 +0200

    foo

--- src/app.cpp
+++ src/app.cpp
@@ -12,48 +12,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    auto s1 = new QSlider(Qt::Horizontal);
-    auto s2 = new QSlider(Qt::Vertical);
-    auto *sb = new QSpinBox;
-
-    auto *w = new QMainWindow;
-
-    auto cw = new QWidget(w);
-
-    auto menu = new QMenuBar(cw);
-
-
-    auto *L = new QHBoxLayout(cw);
-    L->addWidget(s1);
-    L->addWidget(s2);
-    L->addWidget(sb);
-
-    QMenu *file = new QMenu("&File");
-    file->addMenu("Open");
-    file->addMenu("Exit");
-
-    QMenu *Build = new QMenu("&Build");
-    Build->addAction("Rebuild this file");
-    Build->addAction("Rebuild All");
-
-    menu->addMenu(file);
-    menu->addMenu(Build);
-
-    w->setCentralWidget(cw);
-
-    w->show();
-
-    QObject::connect  (s1,SIGNAL(valueChanged(int) ),  sb,SLOT(setValue(int) )   );
-    QObject::connect  (s1,SIGNAL(valueChanged(int) ),  s2,SLOT(setValue(int) )   );
-
-    QObject::connect  (s2,SIGNAL(valueChanged(int) ),  sb,SLOT(setValue(int) )   );
-    QObject::connect  (s2,SIGNAL(valueChanged(int) ),  s1,SLOT(setValue(int) )   );
-
-    QObject::connect  (sb,SIGNAL(valueChanged(int) ),  s1,SLOT(setValue(int) )    );
-    QObject::connect  (sb,SIGNAL(valueChanged(int) ),  s2,SLOT(setValue(int) )    );
-
-//    Win w;
-//    w.show();
+    Win w;
+    w.show();
 
     return QApplication::exec();
 }
--- src/win.cpp
+++ src/win.cpp
@@ -1,10 +1,32 @@
 #include "win.h"
 
+#include <QHBoxLayout>
+#include <qmenu.h>
+#include <QMenuBar>
+#include <QStatusBar>
+
 Win::Win(QWidget *parent) noexcept : QMainWindow(parent) {
 	setWindowTitle(APP_LONG_NAME);
-	setFixedSize(640, 480);
-    btn = new QPushButton("Hello, world!", this);
+
+	auto cw = new QWidget(this);
+
+	auto menu = new QMenuBar(cw);
+	auto file = new QMenu(tr("&File"));
+	auto exitAct = new QAction(tr("E&xit"), this);
+	connect(exitAct, SIGNAL(triggered()), this, SLOT(close()));
+
+	file->addAction(exitAct);
+	menu->addMenu(file);
+
+	btn = new QPushButton(tr("Hello, world!"), this);
 	btn->setDefault(true);
-	setCentralWidget(btn);
+
 	connect(btn, SIGNAL(clicked()), this, SLOT(close()));
+
+	auto *l = new QHBoxLayout(cw);
+	l->addWidget(btn);
+
+	setCentralWidget(cw);
+
+	statusBar()->showMessage(tr("Ready"), 2000);
 }
